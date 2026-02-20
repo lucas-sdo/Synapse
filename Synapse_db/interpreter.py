dc4c55b8cb3881fa842b3b0e06bd3b7936c3e30a
Lucas S. de Oliveira <lucas.sdo.cyber@gmail.com>
2026-02-20 20:45:08 -0300

    feat: Início do desenvolvimento das linhas(para salvar dados) + implementação total da estruturação do db. Beggening of rows development(for saving data) + total db structure implementation.

--- Synapse_db/interpreter.py
+++ Synapse_db/interpreter.py
@@ -7,13 +7,15 @@ class Read:
             return [linha.strip() for linha in f if linha.strip()]
 
 
-class Interpreter:
+class Executer:
     def __init__(self, lines: list):
+        self.current_id: int = 0
         self.lines: list = lines
+        self.info_to_write = b''
         self.cols = dict()
         self.tabels = dict()
 
-    def find_operation(self):
+    def execute(self):
         for line in self.lines:
             line_parts = line.split('/', 2)
 
@@ -22,8 +24,14 @@ class Interpreter:
                     self.cols[line_parts[1]] = line_parts[2]
                 case 'tab':
                     self.create_table(line_parts)
+                    self.info_to_write += Writer(
+                        self.cols, self.tabels, self.current_id).write_building()
                 case 'commit':
-                    Writer(self.cols, self.tabels, line_parts).write_tables()
+                    with open(line_parts[2], "wb") as f:
+                        f.write(self.info_to_write)
+                case 'add':
+                    Writer(self.cols, self.tabels,
+                           self.current_id).write_info(line_parts)
                 case 'com':
                     pass
 
@@ -39,39 +47,46 @@ class Interpreter:
                     col_buffer.append(col)
                 else:
                     print("Here there will be a standardized error msg.")
-
             self.tabels[name] = col_buffer
         else:
             print("Here there will be a standardized error msg.")
 
 
 class Writer:
-    def __init__(self, cols: dict, tabs: dict, db_path: str):
+    def __init__(self, cols: dict, tabs: dict, current_id: int):
+        self.current_id: int = current_id
         self.cols: dict = cols
         self.tabs: dict = tabs
-        self.db_path: str = db_path
         self.type_codes = {
             "bool": b"\x01",
             "int": b"\x02",
             "float": b"\x03",
             "str": b"\x04",
+            "id": b"\x05",
         }
 
-    def write_tables(self):
-        print(self.cols)
-        with open(self.db_path[2], "wb") as f:
-            cols_to_write = b'\x2f\x73\x79\x6e\x2f\n'
-            tabs_to_write = b''
-            for col_name, col_type in self.cols.items():
-                cols_to_write += col_name.encode('utf-8') + \
-                    b"\x00" + self.type_codes[col_type] + b"\n"
+    def write_building(self):
+        cols_to_write = b'\x2f\x73\x79\x6e\x2f\n'
+        tabs_to_write = b''
+        for col_name, col_type in self.cols.items():
+            cols_to_write += col_name.encode('utf-8') + \
+                b"\x00" + self.type_codes[col_type] + b"\n"
 
-            for tab_name, tab_cols in self.tabs.items():
-                tabs_to_write += tab_name.encode('utf-8') + b'\x2f'
-                for col in tab_cols:
-                    tabs_to_write += col.encode('utf-8') + b'\x00'
+        for tab_name, tab_cols in self.tabs.items():
+            tabs_to_write += tab_name.encode('utf-8') + b'\x2f'
+            for col in tab_cols:
+                tabs_to_write += col.encode('utf-8') + b'\x00'
+            tabs_to_write += b"\n"
+        return cols_to_write + tabs_to_write
 
-            f.write(cols_to_write + tabs_to_write)
+    def write_info(self, line_parts):
+        table = line_parts[1]
+        info = line_parts[2]
+        if info.startswith('(') and info.endswith(')'):
+            info = info[1:-1].split('/')
+            row_to_write = b'\x72\x6f\x77\x2f' + table.encode('utf-8')
+        else:
+            print("Here there will be a standardized error msg.")
 
 
-Interpreter(Read('Synapse_db/commands.sync').read()).find_operation()
+Executer(Read('Synapse_db/commands.sync').read()).execute()
